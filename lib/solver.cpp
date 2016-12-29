c3ec5acf09b4c88f5b1a706c4ab0feeefe3a42e6
Joe Cross <joe.mcross@gmail.com>
2016-12-29 14:25:30 -0800

    Solver works for VERY_FAST factors but not FAST (40 bits)

--- lib/solver.cpp
+++ lib/solver.cpp
@@ -1,18 +1,28 @@
+#include <bitset>
 #include "solver.h"
 #include "bits.h"
-
 
 // R_i = diagonal_multiply(state.p, state.q, i)
 // c_i = (r_(i-1) >> 1) = carry(r_(i-1))
 // r_i = R_i + (r_(i-1) >> 1) = R_i + c_i
 // s_i = r_i & 1 = remainder(r_i)
 
-inline bool advance (Partial &state) {
-    // 0. Don't advance if we're out of permutations at this index
-    if (state.permutations[state.pos] > 1) { return false;}
+inline uint32_t apply_permutation(Partial &state,
+                              uint32_t R_i, uint32_t c_i, uint32_t s_i,  // x
+                              uint8_t permute_index) {
+    uint64_t x = R_i ^ c_i ^ s_i;
+    state.p = PERMUTE_FUNCTIONS[(x&1)<<2 | permute_index<<1 | 0](state.p, state.pos);
+    state.q = PERMUTE_FUNCTIONS[(x&1)<<2 | permute_index<<1 | 1](state.q, state.pos);
+    return R_i + bit_at(state.p, state.pos) + bit_at(state.q, state.pos);
+};
 
-    // 1. Grab the next permutation index for this position, then increment for next advance
+inline bool advance (Partial &state) {
     uint8_t permutation = state.permutations[state.pos];
+
+    // 0. Don't advance if we're out of permutations at this index
+    if (permutation > 1) { return false;}
+
+    // 1. Increment permutation index for next advance
     state.permutations[state.pos]++;
 
     // 2. Calculate the new carry for this permutation.
@@ -23,46 +33,53 @@ inline bool advance (Partial &state) {
     // Luckily, p_0 and q_0 are always 1 which lets us simply add the permutation to
     // finding the new carry.
 
-    // TODO is this supposed to be state.pos - 2 or state.pos - 1?
-    uint32_t R_i = diagonal_multiply(state.p >> 1, state.q >> 1, (uint8_t) (state.pos - 2));
+    uint32_t R_i;
+    if (state.pos > 1) {
+        R_i = diagonal_multiply(state.p >> 1, state.q >> 1, (uint8_t) (state.pos - 2));
+    } else {
+        // TODO cleaner way to handle the edge case of no inner terms between p_i, p_0
+        R_i = 0;
+    }
 
     // carry(r_(i_1))
     // invariants: state.pos >= 1 and state.carry[0] is 0
     uint32_t c_i = state.carry[state.pos - 1];
 
     // get the goal's bit at this position
-    uint32_t s_i = bit_at(state.goal, state.pos);
+    uint32_t s_i = bit_at((uint32_t) state.goal, state.pos);
 
     // find this permutation's p_i, q_i and add them to R_i
-    R_i += apply_permutation(R_i ^ c_i ^ s_i, permutation, state.p, state.q, state.pos);
+    R_i = apply_permutation(state, R_i, c_i, s_i, permutation);
     // carry over all but s_i for the next iteration
     state.carry[state.pos] = carry(R_i + c_i);
 
-    // 3. Move to the next digit, clearing any previous permutations.
-    state.pq = state.p * state.q;
-    state.pos++;
+    return true;
+}
+
+inline void reverse(Partial &state) {
+    // TODO | be careful of overflow when pos (uint8_t) can wrap on bit length (p, q > 254 bits)
+    // TODO | otherwise, we'll end up zeroing the first digit (p_0, q_0)
+    state.p = zero_at(state.p, state.pos);
+    state.q = zero_at(state.q, state.pos);
     state.permutations[state.pos] = 0;
+    state.pos--;
 }
 
-inline bool check(Partial &state) {
-    return state.pq == state.goal && state.p != 1 and state.q != 1;
-}
-
-Solution solve(uint32_t goal) {
+Solution solve(uint64_t goal) {
     Partial state = {goal};
-    // Force an exit as soon as the solver falls back to the first digit
-    // there's only one option for p_0, q_0
-    state.permutations[0] = 2;
+    uint64_t pq = 0;
 
-
-    while (state.pos < 32) {
-        // TODO advance can fail because permutations are exhausted.
-        // TODO handle that here
+    while (state.pos > 0) {
         if (!advance(state)) {
-            state.pos--;
+            reverse(state);
             continue;
         }
-        if (check(state)) return Solution{.p=state.p, .q=state.q};
+
+        pq = (uint64_t) state.p * (uint64_t) state.q;
+
+        if (pq > goal) continue;  // Try next permutation, same position
+        else if (pq < goal) state.pos++; // Move on to next digit
+        else if (state.p != 1 && state.q != 1) return Solution{.p=state.p, .q=state.q};  // Non-trivial decomposition
     }
     return Solution{0, 0};
 }
--- lib/solver.h
+++ lib/solver.h
@@ -2,17 +2,17 @@
 #include "constants.h"
 
 struct Partial {
-    uint32_t p, q, pq;
+    uint32_t p, q;
     uint8_t permutations[32];
     uint8_t pos;
     uint32_t carry[32];
-    uint32_t goal;
-    Partial(uint32_t g) : p{1}, q{1}, pq{1}, pos{1}, permutations{0}, carry{0}, goal{g} {};
+    uint64_t goal;
+    Partial(uint64_t g) : p{1}, q{1}, pos{1}, permutations{0}, carry{0}, goal{g} {};
 };
 
 typedef struct {
-    uint32_t p;
-    uint32_t q;
+    uint64_t p;
+    uint64_t q;
 } Solution;
 
-Solution solve(uint32_t product);
+Solution solve(uint64_t product);
