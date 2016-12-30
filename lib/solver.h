462d1f136911cc8b2276cc67f02339b679c77576
Joe Cross <joe.mcross@gmail.com>
2016-12-30 00:04:47 -0800

    Store goal as array to get s_i with one index

--- lib/solver.h
+++ lib/solver.h
@@ -5,8 +5,16 @@ struct Partial {
     uint8_t permutations[64];
     uint8_t pos;
     uint64_t carry[64];
-    uint64_t goal;
-    Partial(uint64_t g) : p{1}, q{1}, pos{1}, permutations{0}, carry{0}, goal{g} {};
+    uint8_t goal[64];
+    Partial(uint64_t goal) : p{1}, q{1}, pos{1}, permutations{0}, carry{0}
+    {
+        int i = 0;
+        while(i <= 63) {
+            this->goal[i] = (uint8_t) (goal & 1);
+            goal >>= 1;
+            i++;
+        }
+    };
 };
 
 typedef struct {
