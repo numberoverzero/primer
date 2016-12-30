4d5f98b3295d1634670ad9808953b8693593b850
Joe Cross <joe.mcross@gmail.com>
2016-12-30 00:06:20 -0800

    perf: apply_permutation uses PERMUTE_OFFSET and state.goal[pos]

--- lib/solver.cpp
+++ lib/solver.cpp
@@ -1,49 +1,32 @@
 #include "solver.h"
 #include "bits.h"
 
-// R_i = diagonal_multiply(state.p, state.q, i)
+// R_i = diagonal_multiply(state.p >> 1, state.q >> 1, i - 2)
 // c_i = (r_(i-1) >> 1) = carry(r_(i-1))
 // r_i = R_i + (r_(i-1) >> 1) = R_i + c_i
 // s_i = r_i & 1 = remainder(r_i)
 
 inline uint64_t apply_permutation(Partial &state,
-                              uint64_t c_i, uint64_t s_i,  // x
+                              uint64_t c_i, uint8_t s_i,  // x
                               uint8_t permute_index) {
-    // when working on r_i, we're finding p_i, q_i
-    // so we need to remove these factors from R_i (that is, p_i*q_0 and q_i*p_0)
-    // and therefore drop the right-most digits (p_0, q_0) that they would be multiplied by.
-    // Luckily, p_0 and q_0 are always 1 which lets us simply add the permutation to
-    // finding the new carry.
     uint64_t R_i = diagonal_multiply(state.p >> 1, state.q >> 1, (uint8_t) (state.pos - 2));
-    uint64_t x = R_i ^ c_i ^ s_i;
-    state.p = PERMUTE_FUNCTIONS[(x&1)<<2 | permute_index<<1 | 0](state.p, state.pos);
-    state.q = PERMUTE_FUNCTIONS[(x&1)<<2 | permute_index<<1 | 1](state.q, state.pos);
-    return R_i + bit_at(state.p, state.pos) + bit_at(state.q, state.pos);
+    uint8_t x = (uint8_t) ((R_i ^ c_i ^ s_i) & 1);
+    uint8_t index = (uint8_t) ((x & 1) << 2 | permute_index << 1);
+    // TODO | this should not need to lookup a function, just set to 0 or 1 based on index
+    // TODO | see https://graphics.stanford.edu/~seander/bithacks.html#ConditionalSetOrClearBitsWithoutBranching
+    state.p = PERMUTE_FUNCTIONS[index](state.p, state.pos);
+    state.q = PERMUTE_FUNCTIONS[index + 1](state.q, state.pos);
+    return R_i + PERMUTE_OFFSET[index>>1];
 };
 
 inline bool advance (Partial &state) {
-    uint8_t permutation = state.permutations[state.pos];
-
-    // 0. Don't advance if we're out of permutations at this index
+    uint8_t permutation = state.permutations[state.pos]++;
     if (permutation > 1) { return false;}
 
-    // 1. Increment permutation index for next advance
-    state.permutations[state.pos]++;
-
-    // 2. Calculate the new carry for this permutation.
-
-    // carry(r_(i_1))
-    // invariants: state.pos >= 1 and state.carry[0] is 0
     uint64_t c_i = state.carry[state.pos - 1];
-
-    // get the goal's bit at this position
-    uint64_t s_i = bit_at(state.goal, state.pos);
-
-    // find this permutation's p_i, q_i and add them to R_i
+    uint8_t s_i = state.goal[state.pos];
     uint64_t r_i = apply_permutation(state, c_i, s_i, permutation);
-    // carry over all but s_i for the next iteration
     state.carry[state.pos] = (r_i + c_i) >> 1;
-
     return true;
 }
 
@@ -52,13 +35,12 @@ inline void reverse(Partial &state) {
     // TODO | otherwise, we'll end up zeroing the first digit (p_0, q_0)
     state.p &= ~(1ull << state.pos);
     state.q &= ~(1ull << state.pos);
-    state.permutations[state.pos] = 0;
-    state.pos--;
+    state.permutations[state.pos--] = 0;
 }
 
 Solution solve(uint64_t goal) {
     Partial state = {goal};
-    uint64_t pq = 0;
+    uint64_t pq;
 
     while (state.pos > 0) {
         if (!advance(state)) {
