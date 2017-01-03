4005d651bc07627b1b907a68619c47c520676a3a
Joe Cross <joe.mcross@gmail.com>
2017-01-02 16:05:36 -0800

    for testing random patterns

--- applications/scratchpad.cpp
+++ applications/scratchpad.cpp
@@ -5,9 +5,10 @@
 
 
 int main() {
-    uint64_t x = 0b0011110;
+    uint64_t x = 0b1111101110001100001100000110000001100000001100000000110001010101;
     render_bits(x);
     render_bits(reverse(x));
+    render_bits(reverse(x) << 5);
 
     return 0;
 }
